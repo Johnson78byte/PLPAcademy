ab804ccd8b61f6b9c12c5265f3a20d497d51a97c
Johnson78byte <johnson.learning11@gmail.com>
2025-07-29 04:29:33 -0400

    My first file to push in Github

---
+++ calculator.py
@@ -0,0 +1,17 @@
+a = 5
+b= 9
+
+print("The sum of a and b is:", a + b)
+print("The difference of a and b is:", a - b)
+print("The product of a and b is:", a * b)
+print("The quotient of a and b is:", a / b)
+print("The modulus of a and b is:", a % b)
+print("The exponent of a raised to b is:", a ** b)
+print("The floor division of a and b is:", a // b)
+print("The maximum of a and b is:", max(a, b))
+print("The minimum of a and b is:", min(a, b))
+print("The absolute value of a is:", abs(a))
+print("The absolute value of b is:", abs(b))
+print("The rounded value of a is:", round(a))
+print("The rounded value of b is:", round(b))
+print("The square root of a is:", a ** 0.5)
